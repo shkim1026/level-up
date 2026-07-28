3629d0522959f4f2767867ebad085584da4652b0
shkim1026 <60453195+shkim1026@users.noreply.github.com>
2026-07-27 17:09:45 -0700

    replaces favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,17 @@ export const metadata = {
   alternates: {
     canonical: "/",
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
   robots: {
     index: true,
     follow: true,
